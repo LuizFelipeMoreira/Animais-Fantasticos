3d399a3052b11da974428208944fc142e86f9bf2
LuizFelipeMoreira <luizfelipemlds65@gmail.com>
2023-08-31 17:22:43 -0300

    animacao ao scroll refeita

--- js/modules/ScrollAnimar.js
+++ js/modules/ScrollAnimar.js
@@ -2,7 +2,28 @@ export default class ScrollAnima {
   constructor(section) {
     this.sections = document.querySelectorAll(section);
     this.windowMetade = window.innerHeight * 0.6;
-    this.animar = this.animar.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
+  }
+
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const topo = section.offsetTop;
+
+      return {
+        element: section,
+        offset: Math.floor(topo - this.windowMetade),
+      };
+    });
+  }
+
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add("ativo");
+      } else if (item.element.classList.contains("ativo")) {
+        item.element.classList.remove("ativo");
+      }
+    });
   }
 
   animar() {
@@ -18,7 +39,16 @@ export default class ScrollAnima {
   }
 
   init() {
-    this.animar();
-    window.addEventListener("scroll", this.animar);
+    if (this.sections.length) {
+      this.getDistance();
+      this.animar();
+      this.checkDistance();
+      window.addEventListener("scroll", this.checkDistance);
+    }
+    return this;
+  }
+
+  stop() {
+    window.removeEventListener("scroll", this.checkDistance);
   }
 }
